8154ce35efd0dab86e5a2ce3a1ed52b4875b0e64
Swapnil Patel <swapnilpatel357@gmail.com>
2018-06-22 10:00:00 -0700

    fix for Django 1.6+
    no package - defaults
    patterns deprecated, used url() instead.

--- djide/urls.py
+++ djide/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import include, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 #
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', 'djide.views.edit', name='edit'),
     url(r'^tree-data$', 'djide.views.tree_data', name='treedata'),
     url(r'^model-editor', 'djide.views.model_editor', name='modeleditor'),
-)
+]
